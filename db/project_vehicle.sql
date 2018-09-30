546308f13e3e77b841dbec254aa00ce0645774a9
team1itc <team1@itchaiyaphum.com>
2018-10-01 00:07:32 +0700

    db {pk_student(add:tb(std_blood))}

--- db/project_vehicle.sql
+++ db/project_vehicle.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 29, 2018 at 02:01 PM
+-- Generation Time: Sep 30, 2018 at 07:05 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -35,18 +35,31 @@ CREATE TABLE IF NOT EXISTS `pk_student` (
   `std_lname` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `std_pin_id` varchar(13) COLLATE utf8mb4_unicode_ci NOT NULL,
   `std_birthday` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `std_username` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `std_password` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `g_code` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `std_username` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `std_password` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `g_code` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `std_blood` varchar(5) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `pk_student`
 --
 
-INSERT INTO `pk_student` (`std_id`, `std_code`, `std_gender`, `std_prename`, `std_name`, `std_lname`, `std_pin_id`, `std_birthday`, `std_username`, `std_password`, `g_code`) VALUES
-(1, '6139010001', 'ชาย', 'นาย', 'ชาติชาย', 'ชัยภูมิ', '1468890567893', '12/12/1999', 'user01', 'user01', '61390101'),
-(2, '6139010002', 'หญิง', 'นางสาว', 'สมหญิง', 'สกุลไทย', '1468890567894', '12/12/1999', 'user02', 'user02', '61390101');
+INSERT INTO `pk_student` (`std_id`, `std_code`, `std_gender`, `std_prename`, `std_name`, `std_lname`, `std_pin_id`, `std_birthday`, `std_username`, `std_password`, `g_code`, `std_blood`) VALUES
+(1, '6139010001', 'ชาย', 'นาย', 'ชาติชาย', 'ชัยภูมิ', '1468890567893', '12/12/1999', 'user01', 'user01', '61390101', NULL),
+(2, '6139010002', 'หญิง', 'นางสาว', 'สมหญิง', 'สกุลไทย', '1468890567894', '12/12/1999', 'user02', 'user02', '61390101', NULL),
+(3, '1', 'ชาย', 'นาย', 'name', 'lname', '2', 'birth', '', '', '', 'A'),
+(4, '2', 'หญิง', 'นางสาว', 'นิชา', 'เติบ', '2', '5 พ.ค.2540', '', '', '', 'B'),
+(5, '2', 'หญิง', 'นางสาว', 'นิชา', 'เติบ', '2', '5 พ.ค.2540', NULL, NULL, NULL, 'B'),
+(6, '6039010002', 'ชาย', 'นาย', 'ชาญ', 'บุญ', '1369900467391', '24 พ.ย. 2541', NULL, NULL, NULL, 'B'),
+(7, '6039010002', 'ชาย', 'นาย', 'ชาญ', 'บุญ', '1369900467391', '24 พ.ย. 2541', '6039010002', '1369900467391', NULL, 'B'),
+(8, '6039010002', 'ชาย', 'นาย', 'ชาฐ', 'บุญ', '1369900467391', '24 พ.ย. 2541', '6039010002', '13699004673911', '60390101', 'B'),
+(10, '', '', '', '', '', '', '', '', '', '', ''),
+(11, '', '', '', '', '', '', '', '', '', '', ''),
+(12, '1', '', '', '1', '1', '1', '1', '1', '1', '1', ''),
+(13, '', '', '', '', '', '', '', '', '', '', ''),
+(14, '', '', '', '', '', '', '', '', '', '', ''),
+(15, '252525', '', '', '', '', '', '', '252525', '', '', '');
 
 --
 -- Indexes for dumped tables
@@ -66,7 +79,7 @@ ALTER TABLE `pk_student`
 -- AUTO_INCREMENT for table `pk_student`
 --
 ALTER TABLE `pk_student`
-MODIFY `std_id` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `std_id` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
